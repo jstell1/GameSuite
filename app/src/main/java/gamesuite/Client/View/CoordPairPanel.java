0ef68d8ea8f0c1ef4e3eaab27fa8cc00a13d91f1
jstell1 <jstell1234@outlook.com>
2025-08-11 19:22:17 -0700

    changed mouseClicked to mousePressed for better responsiveness

--- app/src/main/java/gamesuite/Client/View/CoordPairPanel.java
+++ app/src/main/java/gamesuite/Client/View/CoordPairPanel.java
@@ -39,7 +39,7 @@ public class CoordPairPanel extends JPanel {
         
         addMouseListener(new MouseAdapter() {
             @Override
-            public void mouseClicked(MouseEvent e) {
+            public void mousePressed(MouseEvent e) {
                 if(listener.getIsBoardEnabled()) {
                     Color currCol = getBackground();
                     if(currCol != Color.YELLOW) {
